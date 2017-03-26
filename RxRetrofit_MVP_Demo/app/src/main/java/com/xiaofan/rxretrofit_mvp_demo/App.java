2e91fd5787183b94ffca202f4888783754945726
fanjianhai <18210836561@163.com>
2017-03-26 19:02:36 +0800

    自定义常见异常

---
+++ RxRetrofit_MVP_Demo/app/src/main/java/com/xiaofan/rxretrofit_mvp_demo/App.java
@@ -0,0 +1,14 @@
+package com.xiaofan.rxretrofit_mvp_demo;
+
+import android.app.Application;
+
+/**
+ * Created by fanjianhai on 2017/3/26.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
